82885224ac564218e81fc379bc0c00070b5b7297
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2024-04-24 15:44:09 +0530

    added signup.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { Route,Routes,Link } from 'react-router-dom'
+import Signup from './components/Signup'
 function App() {
 
   return (
     <>
-    <h1>Welcome to intellifeed</h1>
+    {/* <h1>Welcome to intellifeed</h1> */}
+    <Signup></Signup>
     </>
   )
 }
