e609d3920e0b4545ccfc5aec7aa47b5b91f2e04d
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2024-06-03 14:35:26 +0530

    added loginpage

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import signupBG from '../assets/signupBG.png'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
   return (
-    <div>
-      
+    <div className='h-screen w-screen bg-cover bg-center flex justify-center items-center' style={{ backgroundImage: `url(${signupBG})`,}}>
+    <div className='flex flex-col justify-around items-center bg-gradient-to-b from-[#396B91] to-[#7AB1DB]
+    w-1/4 h-3/5 p-10 rounded-md transform scale-90"'>
+      <h1 className='text-4xl font-bold text-white'>Log in</h1>
+
+      <div>
+        <form action="">
+          <div><input className='m-2 p-2' type="text" placeholder='Enter your username' /></div>
+          <div><input className='m-2 p-2' type="text" placeholder='Enter your password' /></div>
+        </form>
+      </div>
+
+      <div className='flex flex-col justify-center items-center'>
+        <button className='font-sans bg-black text-white border-none px-4 py-2 m-2'>Login</button>
+        <Link className='underline' to="/Signup">Dont have an account? Signup.</Link>
+      </div>
     </div>
+  </div>
   )
 }
