a3d0338a53766ea0654026aa9f57a5c68a1514e9
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2024-06-08 23:38:43 +0530

    added signup functionality

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,18 +1,46 @@
 const express = require('express')
 require('dotenv').config()
 const router = express.Router()
+const User = require('./Models/users');
+const bcrypt = require('bcrypt'); 
 
-router.get('/newsAPIorg', (req, res) => {
-    fetch(`https://newsapi.org/v2/everything?q=keyword&apiKey=${process.env.NEWSapiORG}`)
-        .then(response =>response.json())
-        .then(data =>{
-            console.log(data)
-            res.send(data);
-        })
-        .catch(err =>{
-            res.send(err);
-        });
-});
+router.post('/signup', async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+  
+      if (!username || !email || !password) {
+        return res.status(400).json({ message: 'Please fill in all fields.' });
+      }
+      const existingEmail = await User.findOne({ email });
+      const existingUsername = await User.findOne({ username });
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
 
+      let errorMessage = '';
+      if (existingEmail && existingUsername) {
+          errorMessage = 'Username and email already in use.';
+      } else if (existingEmail) {
+          errorMessage = 'Email already in use.';
+      } else if (existingUsername) {
+          errorMessage = 'Username already in use.';
+      } 
+
+      if (errorMessage) {
+          return res.status(400).json({ message: errorMessage });
+      }
+  
+      const newUser = new User({
+        username,
+        email,
+        password: hashedPassword, 
+      });
+
+    const savedUser = await newUser.save();
+      res.status(201).json({ message: 'User created successfully!', user: savedUser.toJSON({ virtuals: true }) }); 
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Server error. Please try again later.' });
+    }
+  });
 
 module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
+app.use(cors()); 
 const port = process.env.PORT || 3000;
 const mongoose=require('mongoose');
 require('dotenv').config()
