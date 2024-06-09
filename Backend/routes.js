3b1ce946142899ac95cef057f8e418ed2b3e0dbb
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2024-06-09 14:36:23 +0530

    updated routes.js

--- Backend/routes.js
+++ Backend/routes.js
@@ -7,6 +7,10 @@ const mongoose=require('mongoose');
 const db =mongoose.connection;
 const mongoURI = process.env.MONGODB_URI
 mongoose.connect(mongoURI);
+const bcrypt = require('bcrypt'); 
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 router.get('/mongoDBstatus',(req,res)=>{
   res.send(`Database Connection Status:${db?'Connected':'Disconnected'}`);
