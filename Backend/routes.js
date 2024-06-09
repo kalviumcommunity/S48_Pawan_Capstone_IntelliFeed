603550a1a1e3e65ff3aac54fb15f7cc81a7ed730
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2024-06-09 13:44:58 +0530

    updated routes.js

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,6 +1,13 @@
 const express = require('express')
 require('dotenv').config()
 const router = express.Router()
+const User = require('./Models/users');
+const summarizedarticles = require("./Models/summarisedarticles")
+const mongoose=require('mongoose');
+const db =mongoose.connection;
+const mongoURI = process.env.MONGODB_URI
+mongoose.connect(mongoURI);
+
 
 
 module.exports = router;
