4e669d7a853f9802c4e7dcd86ecaf025c951c9e1
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2024-06-10 23:21:40 +0530

    updated

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -41,7 +41,7 @@ export default function Login() {
           <div><input className='m-2 p-2' type="text" placeholder='Enter your username'
                value={username}
                onChange={(e) => setUsername(e.target.value)} /></div>
-          <div><input className='m-2 p-2' type="text" placeholder='Enter your password'
+          <div><input className='m-2 p-2' type="password" placeholder='Enter your password'
                value={password}
                onChange={(e) => setPassword(e.target.value)} /></div>
           <div className='flex flex-col justify-center items-center'>
